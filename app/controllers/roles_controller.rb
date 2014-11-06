58be20d658393c8e4268a3533484bd3c00952dfd
Raghu Betina <rbetina@github.com>
2014-11-06 14:29:48 -0600

    Generated resources

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,52 @@
+class RolesController < ApplicationController
+  def index
+    @roles = Role.all
+  end
+
+  def show
+    @role = Role.find(params[:id])
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new
+    @role.character_name = params[:character_name]
+    @role.movie_id = params[:movie_id]
+    @role.actor_id = params[:actor_id]
+
+    if @role.save
+      redirect_to "/roles", :notice => "Role created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @role = Role.find(params[:id])
+  end
+
+  def update
+    @role = Role.find(params[:id])
+
+    @role.character_name = params[:character_name]
+    @role.movie_id = params[:movie_id]
+    @role.actor_id = params[:actor_id]
+
+    if @role.save
+      redirect_to "/roles", :notice => "Role updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @role = Role.find(params[:id])
+
+    @role.destroy
+
+    redirect_to "/roles", :notice => "Role deleted."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,72 @@
 Rails.application.routes.draw do
+  # Routes for the Role resource:
+  # CREATE
+  get('/roles/new', { :controller => 'roles', :action => 'new' })
+  get('/create_role', { :controller => 'roles', :action => 'create' })
+
+  # READ
+  get('/roles', { :controller => 'roles', :action => 'index' })
+  get('/roles/:id', { :controller => 'roles', :action => 'show' })
+
+  # UPDATE
+  get('/roles/:id/edit', { :controller => 'roles', :action => 'edit' })
+  get('/update_role/:id', { :controller => 'roles', :action => 'update' })
+
+  # DELETE
+  get('/delete_role/:id', { :controller => 'roles', :action => 'destroy' })
+  #------------------------------
+
+  # Routes for the Movie resource:
+  # CREATE
+  get('/movies/new', { :controller => 'movies', :action => 'new' })
+  get('/create_movie', { :controller => 'movies', :action => 'create' })
+
+  # READ
+  get('/movies', { :controller => 'movies', :action => 'index' })
+  get('/movies/:id', { :controller => 'movies', :action => 'show' })
+
+  # UPDATE
+  get('/movies/:id/edit', { :controller => 'movies', :action => 'edit' })
+  get('/update_movie/:id', { :controller => 'movies', :action => 'update' })
+
+  # DELETE
+  get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
+  #------------------------------
+
+  # Routes for the Actor resource:
+  # CREATE
+  get('/actors/new', { :controller => 'actors', :action => 'new' })
+  get('/create_actor', { :controller => 'actors', :action => 'create' })
+
+  # READ
+  get('/actors', { :controller => 'actors', :action => 'index' })
+  get('/actors/:id', { :controller => 'actors', :action => 'show' })
+
+  # UPDATE
+  get('/actors/:id/edit', { :controller => 'actors', :action => 'edit' })
+  get('/update_actor/:id', { :controller => 'actors', :action => 'update' })
+
+  # DELETE
+  get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
+  #------------------------------
+
+  # Routes for the Director resource:
+  # CREATE
+  get('/directors/new', { :controller => 'directors', :action => 'new' })
+  get('/create_director', { :controller => 'directors', :action => 'create' })
+
+  # READ
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
+
+  # UPDATE
+  get('/directors/:id/edit', { :controller => 'directors', :action => 'edit' })
+  get('/update_director/:id', { :controller => 'directors', :action => 'update' })
+
+  # DELETE
+  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
